5046b6fa1ac4e33a1ade69f1aa064996abf0cafd
avi970 <80572589+avi970@users.noreply.github.com>
2021-03-14 16:50:36 +0530

    Update Inter_java_2.java

--- Inter_java_2.java
+++ Inter_java_2.java
@@ -4,6 +4,28 @@ public class Inter_java_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		char ch=65;
+		
+	
+		for(int j=0;j<n;j++) {
+			int i=0;
+			
+		while(i<2*(n-j)) {
+			System.out.print(" ");
+			i++;
+		}
+		for(int k=0;k<=j;k++) {
+			System.out.print(ch+++" ");
+		
+		
+
+	}
+		System.out.println();
+		
+
+}
 
 	}
 
